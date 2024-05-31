87ca7c793f526a947038b3c1a9be8cf745c2dd17
W-Faria-M <gabriel.barranco@sptech.school>
2024-05-31 18:25:51 -0300

    Primeira tentativa de conexão com o banco de dados

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -2,16 +2,16 @@ var usuarioModel = require("../models/usuarioModel");
 var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
-    var email = req.body.emailServer;
+    var usuario = req.body.usuarioServer;
     var senha = req.body.senhaServer;
 
-    if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+    if (usuario == undefined) {
+        res.status(400).send("Seu usuário está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está indefinida!");
     } else {
 
-        usuarioModel.autenticar(email, senha)
+        usuarioModel.autenticar(usuario, senha)
             .then(
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
@@ -20,13 +20,13 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
+                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0])
                             .then((resultadoAquarios) => {
                                 if (resultadoAquarios.length > 0) {
                                     res.json({
                                         id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
+                                        usuario: resultadoAutenticar[0].usuario,
                                         senha: resultadoAutenticar[0].senha,
                                         aquarios: resultadoAquarios
                                     });
@@ -35,7 +35,7 @@ function autenticar(req, res) {
                                 }
                             })
                     } else if (resultadoAutenticar.length == 0) {
-                        res.status(403).send("Email e/ou senha inválido(s)");
+                        res.status(403).send("nome e/ou senha inválido(s)");
                     } else {
                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
                     }
@@ -54,23 +54,33 @@ function autenticar(req, res) {
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
+    var usuario = req.body.usuarioServer;
     var senha = req.body.senhaServer;
-    var empresaId = req.body.empresaServer;
+    var estado = req.body.estadoServer;
+    var idade = req.body.idadeServer;
+    var filhos = req.body.filhosServer;
+    var filhosPlan = req.body.filhosPlanServer;
+
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+    } else if (usuario == undefined) {
+        res.status(400).send("Seu usuario está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (empresaId == undefined) {
-        res.status(400).send("Sua empresa está undefined!");
+    } else if (estado == undefined) {
+        res.status(400).send("Seu estado está undefined!");
+    } else if (idade == undefined) {
+        res.status(400).send("Sua idade está undefined!");
+    } else if (filhos == undefined) {
+        res.status(400).send("Nº de filhos está undefined!");
+    } else if (filhosPlan == undefined) {
+        res.status(400).send("Nº de filhos planejados está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, empresaId)
+        usuarioModel.cadastrar(nome, usuario, senha, estado, idade, filhos, filhosPlan)
             .then(
                 function (resultado) {
                     res.json(resultado);
